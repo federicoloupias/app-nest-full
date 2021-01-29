9829fc93b9e977ff4ee99d7749235c2c3073e6cc
20383593140 <floupias@buenosaires.gob.ar>
2021-01-29 18:44:52 -0300

    se agrega dockerfile

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,21 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NoticeModule } from './notice/notice.module';
-import { HnModule } from './hn/hn.module'
+import { HnModule } from './hn/hn.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import 'dotenv/config';
 
-const URL = process.env.MONGO_URI
+// const URL = `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:27017/${process.env.DATABASE_NAME}`;
 
+const hostdb = process.env.MONGO_URL || 'localhost';
+
+const URL = `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${hostdb}:27017/admin`;
+
+console.log('URL', URL);
 @Module({
   imports: [
-  
-  MongooseModule.forRoot(URL,
-      {
-        useNewUrlParser: true
-      }), NoticeModule, HnModule,
-      ScheduleModule.forRoot()
-
+    MongooseModule.forRoot(URL, {
+      useNewUrlParser: true,
+    }),
+    NoticeModule,
+    HnModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || '5120';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log(`App listen on port: ${PORT}`)
   await app.listen(PORT);
 }
 bootstrap();
